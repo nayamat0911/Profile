5d67831544b0ecafbee8c369f20cdf340ee3f329
Nayamat Ullah <nayamat0911@gmail.com>
2022-04-15 23:54:48 +0600

    add new file

--- hero/views.py
+++ hero/views.py
@@ -1,11 +1,10 @@
-from urllib import request
 from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
 def Hero(request):
     program_context={
-        'title':'Home',
+        'title':'Home page',
         'text':'My Home page for profile',
        
 
